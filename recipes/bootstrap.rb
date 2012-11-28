0893f3b07db41b23c85b714dc1dfc80f6b01c5fa
Jeremy Seago <seagoj@gmail.com>
2012-11-28 10:21:47 -0600

    Commit dotfiles

--- recipes/bootstrap.rb
+++ recipes/bootstrap.rb
@@ -5,7 +5,7 @@ unless(File.exists?(lib))
 	Dir.mkdir(lib)
 end
 
-execute 'git-bootstrap'
+execute 'git-bootstrap' do
 	command "git clone git://github.com/twitter/bootstrap.git #{lib}/bootstrap"
 end
 
