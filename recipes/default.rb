32612035bb4d75839642fda1ae77a14645633705
Jeremy Seago <seagoj@gmail.com>
2013-08-18 19:31:31 -0500

    Correct path to nginx config

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,14 +1,14 @@
-  package 'nginx'
+package 'nginx'
   
-  service 'nginx' do
-    supports :status => true, :restart => true, :reload => true
-    action :enable
-  end
+service 'nginx' do
+  supports :status => true, :restart => true, :reload => true
+  action :enable
+end
   
-  cookbook_file '/etc/nginx/sites-available/default' do
-    source "nginx.conf"
-    mode "0777"
-  end
+cookbook_file '/etc/nginx/nginx.conf'
+  source "nginx.conf"
+  mode "0777"
+end
   
 service 'nginx' do
   supports :status => true, :restart => true, :reload => true
